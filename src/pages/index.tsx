c444d4896fab66745ce623b6ff3d3a94d31cc8e8
Ushira Dineth <ushiradineth@gmail.com>
2022-12-27 01:36:27 +0530

    update dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,12 +36,16 @@ const Index: NextPage = () => {
         <meta name="google-site-verification" content="WqjiADJh02W0ssceX3ZwKlqRFhVgDEEPUQjG8au1k80" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16 sm:gap-12 ">
+      <main className="grid min-h-screen place-items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="grid place-items-center gap-6">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Clone<span className="text-[hsl(280,100%,70%)]">gram</span>
           </h1>
-          <p className="text-sm text-white sm:text-2xl">Made with NextJS, TypeScript, tRPC, Zod and TailwindCSS</p>
+          <div className="grid place-items-center text-sm text-white sm:text-2xl">
+            <h1>Made with NextJS, TypeScript, NextAuth, tRPC, Zod and TailwindCSS</h1>
+            <h1>Database and storage on Supabase</h1>
+            <h1>Hosted on Vercel</h1>
+          </div>
           <Auth />
         </div>
       </main>
