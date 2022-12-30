034a2c54aa8ffabc36807cf2b26c7d0d77497101
Ushira Dineth <ushiradineth@gmail.com>
2022-12-30 22:38:29 +0530

    change negative behaviour

--- src/components/OptionMenu.tsx
+++ src/components/OptionMenu.tsx
@@ -18,7 +18,7 @@ interface itemType {
 
 const OptionMenu = (props: itemType) => {
   return (
-    <div className="fixed top-0 left-0 z-20 h-screen w-screen bg-black bg-opacity-30" onClick={() => props.onClickNegative}>
+    <div className="fixed top-0 left-0 z-20 h-screen w-screen bg-black bg-opacity-30">
       <div className={"absolute top-1/2 left-1/2 z-20 h-auto w-[300px] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl shadow-[0px_0px_10px_rgba(0,0,0,0.7)] md:w-[400px] " + props.theme.tertiary}>
         <div className="grid w-full grid-flow-row place-items-center border-b-[1px] border-gray-300 py-8 font-semibold">
           <p className="select-none">{props.title}</p>
