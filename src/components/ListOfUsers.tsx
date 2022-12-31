9bedcabf364e1b1531dc31f2996d22d4eba2e0f8
Ushira Dineth <ushiradineth@gmail.com>
2022-12-31 15:21:39 +0530

    routing to profile onclick and ui fixes

--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -2,6 +2,7 @@ import { AiOutlineClose } from "react-icons/ai";
 import { BiUserPlus } from "react-icons/bi";
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 interface itemType {
   viewport: string;
@@ -16,6 +17,7 @@ interface itemType {
     UserName: string;
     UserHandle: string;
     UserImage: string;
+    UserFollowing: boolean;
   }[];
   title: string;
   UserText: string;
@@ -23,20 +25,22 @@ interface itemType {
 }
 
 const ListOfUsers = (props: itemType) => {
+  const router = useRouter();
+
   return (
     <div className="fixed top-0 left-0 z-20 h-screen w-screen select-none bg-black bg-opacity-30" onClick={() => props.onClickNegative}>
       <div className={"absolute top-1/2 left-1/2 z-20 w-[400px] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl shadow-[0px_0px_10px_rgba(0,0,0,0.7)] " + props.theme.tertiary}>
         <>
-          <div className="grid w-full grid-flow-row place-items-center border-b border-gray-300 py-4 font-semibold">
+          <div className="grid w-full grid-flow-row place-items-center border-b-2 border-gray-300 py-4 font-semibold">
             <p className="select-none text-xl">{props.title}</p>
             <AiOutlineClose onClick={props.onClickNegative} className="fixed top-6 right-6 scale-150 hover:cursor-pointer" />
           </div>
           {props.users.length > 0 ? (
             props.users.map((user, index) => {
               return (
-                <div key={index} className={"flex h-12 w-full items-center justify-center p-10 " + (index !== props.users.length - 1 && " border-b ") + props.theme.tertiary}>
-                  <Image className={"w-12 rounded-full"} src={user.UserImage} height={props.viewport == "Mobile" ? 96 : 160} width={props.viewport == "Mobile" ? 96 : 160} alt="Profile Picture" priority />
-                  <div className="m-4 flex flex-col gap-1 truncate">
+                <div key={index} className={"flex h-12 w-full items-center justify-center p-10 " + (index !== props.users.length - 1 && " border-b ")}>
+                  <Image className={"w-12 cursor-pointer rounded-full"} onClick={() => router.push({ pathname: "/" + user.UserHandle })} src={user.UserImage} height={props.viewport == "Mobile" ? 96 : 160} width={props.viewport == "Mobile" ? 96 : 160} alt="Profile Picture" priority />
+                  <div className="m-4 flex w-full cursor-pointer flex-col gap-1 truncate" onClick={() => router.push({ pathname: "/" + user.UserHandle })}>
                     <div>{user.UserHandle}</div>
                     <div>{user.UserName}</div>
                   </div>
@@ -47,9 +51,9 @@ const ListOfUsers = (props: itemType) => {
               );
             })
           ) : (
-            <div className={"flex flex-col items-center justify-center p-8 rounded-2xl " + props.theme.tertiary}>
+            <div className={"flex flex-col items-center justify-center rounded-2xl p-8 " + props.theme.tertiary}>
               <div className="flex flex-col items-center justify-center p-4">
-                <div className="h-32 w-32 mb-4 grid place-items-center rounded-full border-2">
+                <div className="mb-4 grid h-32 w-32 place-items-center rounded-full border-2">
                   <BiUserPlus className="scale-x-[-6] scale-y-[6] transform" />
                 </div>
                 <div className="text-xl">{props.title}</div>
