9f4f24794c4775390c44ca4e7298d098f36c9c3d
Ushira Dineth <ushiradineth@gmail.com>
2023-01-02 14:10:57 +0530

    fixed active animation

--- src/components/NavBarItem.tsx
+++ src/components/NavBarItem.tsx
@@ -6,8 +6,6 @@ interface itemType {
   IconOnClick?: JSX.Element;
   Text?: string;
   ID: string;
-  active: string;
-  setActive: (params: unknown) => unknown;
   onClickHandler?: () => unknown;
   viewport: string;
   theme: {
@@ -17,26 +15,15 @@ interface itemType {
     tertiary: string;
     accent: string;
   };
+  active?: boolean;
 }
 
 const NavBarItem = (props: itemType) => {
-  const [click, setClick] = useState(false);
   const [hover, setHover] = useState(false);
 
   return (
-    <div
-      id={props.ID}
-      className={"flex cursor-pointer items-center justify-start p-2 hover:rounded-full " + (props.viewport == "Web" && " w-64 p-2 pl-3 ") + (props.theme.type === "dark" ? ` hover:${props.theme.secondary} ` : ` hover:${props.theme.tertiary} `)}
-      onClick={() => {
-        setClick(true);
-        if (props.onClickHandler) {
-          props.onClickHandler();
-        }
-      }}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      <div className={"transition-all duration-200 " + (hover ? (props.viewport == "Mobile" ? " scale-[1.6] " : " scale-110 ") : props.viewport == "Mobile" ? " scale-150 " : " scale-100 ")}>{typeof props.Icon === "string" ? <Image height={props.viewport == "Mobile" ? 14 : 24} width={props.viewport == "Mobile" ? 14 : 24} className={"rounded-full transition-all duration-200 " + (hover ? (props.viewport == "Mobile" ? " scale-[1.6] " : " scale-110 ") : props.viewport == "Mobile" ? " scale-150 " : " scale-100 ") + (click && props.theme.type === "dark" ? (props.viewport === "Mobile" ? " border-[1px] border-white " : " border-2 border-white ") : props.viewport === "Mobile" ? " border-[1px] border-black " : " border-2 border-black ")} src={props.Icon} alt="Profile Picture" /> : click ? props.IconOnClick : props.Icon}</div>
+    <div id={props.ID} className={"flex cursor-pointer items-center justify-start p-2 hover:rounded-full " + (props.viewport == "Web" && " w-64 p-2 pl-3 ") + (props.theme.type === "dark" ? ` hover:${props.theme.secondary} ` : ` hover:${props.theme.tertiary} `)} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} onClick={props.onClickHandler}>
+      <div className={"transition-all duration-200 " + (hover ? (props.viewport == "Mobile" ? " scale-[1.6] " : " scale-110 ") : props.viewport == "Mobile" ? " scale-150 " : " scale-100 ")}>{typeof props.Icon === "string" ? <Image height={props.viewport == "Mobile" ? 14 : 24} width={props.viewport == "Mobile" ? 14 : 24} className={"rounded-full transition-all duration-200 " + (hover ? (props.viewport == "Mobile" ? " scale-[1.6] " : " scale-110 ") : props.viewport == "Mobile" ? " scale-150 " : " scale-100 ") + (props.active && (props.theme.type === "dark" ? (props.viewport === "Mobile" ? " border-[1px] border-white " : " border-2 border-white ") : props.viewport === "Mobile" ? " border-[1px] border-black " : " border-2 border-black "))} src={props.Icon} alt="Profile Picture" /> : props.active ? props.IconOnClick : props.Icon}</div>
       {props.Text && <p className={"ml-2 text-sm font-normal " + (props.viewport == "Web" ? " block " : " hidden ")}>{props.Text}</p>}
     </div>
   );
