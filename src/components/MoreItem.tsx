fcd5c39774a8ed3b7b3f2acdd43d6ccf651dcadc
Ushira Dineth <ushiradineth@gmail.com>
2023-01-04 19:27:07 +0530

    fixed hover issues

--- src/components/MoreItem.tsx
+++ src/components/MoreItem.tsx
@@ -14,7 +14,7 @@ interface itemType {
 
 const MoreItem = (props: itemType) => {
   return (
-    <div className={"flex cursor-pointer items-center p-2 pr-4 text-sm font-normal " + (!props.last && " border-b-2 border-black ") + ` hover:${props.theme.tertiary} `} onClick={props.onClickHandler}>
+    <div className={"flex cursor-pointer items-center p-2 pr-4 text-sm font-normal " + (!props.last && " border-b-2 border-black ") + (props.theme.type === "dark" ? " hover:bg-zinc-800 " : " hover:bg-zinc-200 ")} onClick={props.onClickHandler}>
       <p className="ml-2 w-[150px] font-semibold">{props.Text}</p>
       <div className="scale-150">{props.Icon}</div>
     </div>
