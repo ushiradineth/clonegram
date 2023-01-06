897d1ab9632551bc05c4c3aaf29c3ae9bf937af1
Ushira Dineth <ushiradineth@gmail.com>
2023-01-07 05:22:55 +0530

    added getuser method for preloading user data

---
+++ src/components/GetUser.tsx
@@ -0,0 +1,28 @@
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { trpc } from "../utils/trpc";
+import Spinner from "./Spinner";
+
+interface itemType {
+  user: any;
+  setUser: (params: any) => any;
+  theme: any;
+  status: any;
+  setStatus: (params: any) => any;
+}
+
+const GetUser = (props: itemType) => {
+  const { data: session, status } = useSession();
+  const user = trpc.user.getUser.useQuery({ id: session?.user?.id || "" }, { enabled: status === "authenticated", retry: false, refetchOnWindowFocus: false });
+
+  useEffect(() => {
+    if (user.isSuccess && props.user !== user) props.setUser(user);
+    if (status !== props.status && status === "loading" && user.isLoading) props.setStatus(status);
+    if (status !== props.status && status === "authenticated" && user.isSuccess) props.setStatus(status);
+    if (status !== props.status && status === "unauthenticated") props.setStatus(status);
+  }, [user.isSuccess, status]);
+  
+  return <Spinner theme={props.theme} />
+};
+
+export default GetUser;
