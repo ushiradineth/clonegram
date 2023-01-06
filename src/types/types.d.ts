54c95aecee34ee30a7508c3e09dd1b9ac0cf0599
Ushira Dineth <ushiradineth@gmail.com>
2023-01-07 05:23:36 +0530

    added UserType

---
+++ src/types/types.d.ts
@@ -0,0 +1,16 @@
+import { type User } from "@prisma/client";
+
+export interface UserType {
+  
+  data: User & {
+    followers: User[];
+    following: User[];
+    posts: User[];
+    blockedby: User[];
+    blocking: User[];
+  };
+  refetch: any;
+  isSuccess: boolean;
+  isLoading: boolean;
+  isError: boolean;
+}
