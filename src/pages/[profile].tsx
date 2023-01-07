d5d9fa744de83917be77002e8b129138fb67aa29
Ushira Dineth <ushiradineth@gmail.com>
2023-01-07 12:45:29 +0530

    improved fetching and cta button

--- src/pages/[profile].tsx
+++ src/pages/[profile].tsx
@@ -47,17 +47,20 @@ const Profile = (props: itemType) => {
     setFollowingMenu(false);
     setIsBlockedBy(false);
     setIsBlocking(false);
+    setOptions(false);
     document.dispatchEvent(new Event("visibilitychange"));
   }, [router.query]);
 
   const follow = trpc.user.follow.useMutation({
-    onSuccess: (data) => {
+    onSuccess: () => {
+      page.refetch();
       setIsFollowing(true);
     },
   });
 
   const unfollow = trpc.user.unfollow.useMutation({
-    onSuccess: (data) => {
+    onSuccess: () => {
+      page.refetch();
       setIsFollowing(false);
     },
   });
@@ -85,13 +88,13 @@ const Profile = (props: itemType) => {
           userfollowing.push(element.handle);
         });
 
-        if (session?.user?.handle !== String(profile)) {
-          page.data?.blocking.forEach((element: { id: string | undefined }) => {
-            if (element.id === session?.user?.id && !isBlocking && !isBlockedBy) setIsBlocking(true);
+        if (props.user.data.handle !== String(profile)) {
+          props.user.data.blockedby.forEach((element: { id: string | undefined }) => {
+            if (element.id === page.data.id && !isBlocking && !isBlockedBy) setIsBlockedBy(true);
           });
 
-          page.data?.blockedby.forEach((element: { id: string | undefined }) => {
-            if (element.id === session?.user?.id && !isBlockedBy && !isBlocking) setIsBlockedBy(true);
+          props.user?.data.blocking.forEach((element: { id: string | undefined }) => {
+            if (element.id === page.data.id && !isBlockedBy && !isBlocking) setIsBlocking(true);
           });
         }
       }
@@ -115,15 +118,15 @@ const Profile = (props: itemType) => {
   }
 
   if (status === "unauthenticated" ? page.isError : session?.user?.handle === String(profile) ? page.isError : page.data?.handle === String(profile) ? false : page.data?.handle !== String(profile)) return <div className={"grid h-screen place-items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-3xl font-light text-white " + (props.viewport == "Web" && session && " ml-72 ") + (props.viewport == "Tab" && session && " ml-16 ")}>User does not exist</div>;
-  if (isBlocking) return <div className={"grid h-screen place-items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-3xl font-light text-white " + (props.viewport == "Web" && session && " ml-72 ") + (props.viewport == "Tab" && session && " ml-16 ")}>This user has blocked you</div>;
-  if (isBlockedBy) return <div className={"grid h-screen place-items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-3xl font-light text-white " + (props.viewport == "Web" && session && " ml-72 ") + (props.viewport == "Tab" && session && " ml-16 ")}>You have blocked this user</div>;
+  if (isBlocking) return <div className={"grid h-screen place-items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-3xl font-light text-white " + (props.viewport == "Web" && session && " ml-72 ") + (props.viewport == "Tab" && session && " ml-16 ")}>You have blocked this user</div>;
+  if (isBlockedBy) return <div className={"grid h-screen place-items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-3xl font-light text-white " + (props.viewport == "Web" && session && " ml-72 ") + (props.viewport == "Tab" && session && " ml-16 ")}>This user has blocked you</div>;
 
   if (!isBlockedBy && !isBlocking)
     return (
       <div>
         {followersMenu && <ListOfUsers viewport={props.viewport} users={followers} userSetter={() => setFollowers} theme={props.theme} onClickNegative={() => setFollowersMenu(false)} title="Followers" userHandle={session?.user?.handle} userID={session?.user?.id} pageID={page.data?.id ? page.data.id : "0"} />}
         {followingMenu && <ListOfUsers viewport={props.viewport} users={following} userSetter={() => setFollowing} theme={props.theme} onClickNegative={() => setFollowingMenu(false)} title="Following" userHandle={session?.user?.handle} userID={session?.user?.id} pageID={page.data?.id ? page.data.id : "0"} />}
-        {options && <ProfileOptions onClickNegative={() => setOptions(false)} theme={props.theme} page={page} setIsBlocking={setIsBlocking} />}
+        {options && <ProfileOptions onClickNegative={() => setOptions(false)} theme={props.theme} page={page as UserType} refetch={props.user.refetch} setIsBlocking={setIsBlocking} />}
         {!session && (
           <div className={"fixed bottom-0 left-0 flex h-12 w-screen items-center justify-center gap-2 " + props.theme.primary}>
             Sign in to Clonegram to see more!
@@ -139,19 +142,42 @@ const Profile = (props: itemType) => {
                 <Image className={"rounded-full " + (props.viewport == "Mobile" ? " mr-2 ml-2 mt-4 h-24 w-24 " : " mr-10 flex w-24 scale-125 justify-center ")} src={page.data?.image ? page.data?.image : ""} height={96} width={96} alt="Profile Picture" priority />
                 <div id="headline" className={"mb-4 mt-6 ml-4 grid grid-flow-row " + (props.viewport != "Mobile" && " h-fit gap-3 ")}>
                   <div id="user-info">
-                    <div className={"flex items-center " + (props.viewport === "Mobile" ? " gap-3 " : " gap-2 ")}>
+                    <div className="flex items-center gap-3">
                       <div id="id" className="max-w-[200px] overflow-hidden text-ellipsis text-xl">
                         {page.data?.handle}
                       </div>
-                      <button id="cta" onClick={() => (session ? (session?.user?.id === page.data?.id ? router.push("/settings") : isFollowing ? unfollowFunc() : followFunc()) : router.push("/"))} className={"mx-2 cursor-pointer rounded-[4px] border-2 py-1 px-2 text-xs font-semibold " + (props.viewport == "Mobile" && " hidden ")}>
-                        {session?.user?.id === page.data?.id ? "Edit profile" : isFollowing ? "Following" : "Follow"}
+                      <button id="cta" disabled={follow.isLoading || unfollow.isLoading} className={"cursor-pointer text-xs font-semibold disabled:cursor-not-allowed " + (props.viewport === "Mobile" && " hidden ") + (follow.isLoading || unfollow.isLoading ? " " : " rounded-[4px] border py-1 px-2 ")} onClick={() => (session ? (session?.user?.id === page.data?.id ? router.push("/settings") : isFollowing ? unfollowFunc() : followFunc()) : router.push("/"))}>
+                        {session?.user?.id === page.data?.id ? (
+                          "Edit profile"
+                        ) : follow.isLoading || unfollow.isLoading ? (
+                          <svg aria-hidden="true" className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                          </svg>
+                        ) : isFollowing ? (
+                          "Following"
+                        ) : (
+                          "Follow"
+                        )}
                       </button>
                       {session?.user?.id === page.data?.id ? <IoMdSettings id="settings" className="scale-150 cursor-pointer" onClick={() => router.push("/settings")} /> : <BsThreeDots className="scale-150 cursor-pointer" onClick={() => (session ? setOptions(true) : router.push("/"))} />}
                     </div>
                   </div>
-                  <div id="cta-mobile" onClick={() => (session ? (session?.user?.id === page.data?.id ? router.push("/settings") : isFollowing ? unfollowFunc() : followFunc()) : router.push("/"))} className={"z-10 mt-2 flex h-fit w-[235px] cursor-pointer items-center justify-center rounded-[4px] border-2 p-2 text-xs font-semibold  " + (props.viewport != "Mobile" && " hidden ")}>
-                    {session?.user?.id === page.data?.id ? "Edit profile" : isFollowing ? "Following" : "Follow"}
-                  </div>
+                  <button id="cta-mobile" disabled={follow.isLoading || unfollow.isLoading} className={"z-10 mt-2 flex h-fit w-[235px] cursor-pointer items-center justify-center rounded-[4px] border-2 p-2 text-xs font-semibold " + (props.viewport != "Mobile" && " hidden ") + (follow.isLoading || unfollow.isLoading ? " " : " rounded-[4px] border py-1 px-2 ")} onClick={() => (session ? (session?.user?.id === page.data?.id ? router.push("/settings") : isFollowing ? unfollowFunc() : followFunc()) : router.push("/"))}>
+                    {session?.user?.id === page.data?.id ? (
+                      "Edit profile"
+                    ) : follow.isLoading || unfollow.isLoading ? (
+                      <svg aria-hidden="true" className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                      </svg>
+                    ) : isFollowing ? (
+                      "Following"
+                    ) : (
+                      "Follow"
+                    )}
+                  </button>
+
                   <div id="stats" className={"grid grid-flow-col gap-2 text-sm font-normal " + (props.viewport == "Mobile" && " hidden ")}>
                     <div className="flex gap-1">
                       <p className="font-semibold">{page.data?.posts.length}</p>
