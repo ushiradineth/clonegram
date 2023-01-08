06c84f2e8cf7c52b9591a1f490899f832a1477bf
Ushira Dineth <ushiradineth@gmail.com>
2023-01-08 12:14:40 +0530

    added memotype for data context

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,7 +1,6 @@
 import { type User } from "@prisma/client";
 
 export interface UserType {
-  
   data: User & {
     followers: User[];
     following: User[];
@@ -16,4 +15,19 @@ export interface UserType {
   isRefetching: boolean;
   isFetching: boolean;
   isFetched: boolean;
+}
+
+export interface ThemeType {
+  type: string;
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  accent: string;
+}
+
+export interface MemoType {
+  user: UserType | null;
+  theme: ThemeType | null;
+  viewport: "Web" | "Tab" | "Mobile" | string;
+  supabase: any | null;
 }
