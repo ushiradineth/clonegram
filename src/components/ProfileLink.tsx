5d30bab14cf1523a80bc3c291a9cc3bf3381139a
Ushira Dineth <ushiradineth@gmail.com>
2023-01-10 00:23:46 +0530

    changed a tags to next link

--- src/components/ProfileLink.tsx
+++ src/components/ProfileLink.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useContext } from "react";
 import { DataContext } from "../pages/_app";
 
@@ -18,14 +19,14 @@ const ProfileLink = (props: itemType) => {
   const data = useContext(DataContext);
   
   return (
-    <a href={props.user.userHandle} onClick={(e) => e.preventDefault()} key={props.index} className={"mt-6 flex h-12 w-full px-6 items-center justify-center"}>
+    <Link href={props.user.userHandle} onClick={(e) => e.preventDefault()} key={props.index} className={"mt-6 flex h-12 w-full px-6 items-center justify-center"}>
       <Image className={"w-12 cursor-pointer rounded-full"} onClick={props.onClickHandler} src={props.user.userImage} height={data?.viewport == "Mobile" ? 96 : 160} width={data?.viewport == "Mobile" ? 96 : 160} alt="Profile Picture" priority />
       <div className="m-4 flex w-full cursor-pointer flex-col gap-1 truncate" onClick={props.onClickHandler}>
         <div>{props.user.userHandle}</div>
         <div>{props.user.userName}</div>
       </div>
       {props.action && props.action}
-    </a>
+    </Link>
   );
 };
 
