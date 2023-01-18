d5005f1ec717a4965b11e90d34d81863bfdeaddf
Ushira Dineth <ushiradineth@gmail.com>
2023-01-18 13:24:30 +0530

    added new queries for comments

--- src/server/trpc/router/post.ts
+++ src/server/trpc/router/post.ts
@@ -20,16 +20,115 @@ export const postRouter = router({
         id: input.id,
       },
       include: {
-        user: true
-      }
+        user: true,
+      },
     });
   }),
 
-  deletePost: protectedProcedure.input(z.object({ id: z.string() })).query(({ input, ctx }) => {
+  deletePost: protectedProcedure.input(z.object({ id: z.string() })).mutation(({ input, ctx }) => {
     return ctx.prisma.post.delete({
-      where: {
-        id: input.id,
-      },
+      where: { id: input.id },
+    });
+  }),
+
+  likePost: protectedProcedure.input(z.object({ postid: z.string(), userid: z.string() })).mutation(async ({ input, ctx }) => {
+    const q1 = await ctx.prisma.post.update({
+      where: { id: input.postid },
+      data: { likes: { connect: { id: input.userid } } },
+    });
+
+    const q2 = await ctx.prisma.user.update({
+      where: { id: input.userid },
+      data: { likes: { connect: { id: input.postid } } },
+    });
+
+    return { q1, q2 };
+  }),
+
+  unlikePost: protectedProcedure.input(z.object({ postid: z.string(), userid: z.string() })).mutation(async ({ input, ctx }) => {
+    const q1 = await ctx.prisma.post.update({
+      where: { id: input.postid },
+      data: { likes: { disconnect: { id: input.userid } } },
+    });
+    const q2 = await ctx.prisma.user.update({
+      where: { id: input.userid },
+      data: { likes: { disconnect: { id: input.postid } } },
+    });
+
+    return { q1, q2 };
+  }),
+
+  savePost: protectedProcedure.input(z.object({ postid: z.string(), userid: z.string() })).mutation(async ({ input, ctx }) => {
+    const q1 = await ctx.prisma.post.update({
+      where: { id: input.postid },
+      data: { saved: { connect: { id: input.userid } } },
+    });
+    const q2 = await ctx.prisma.user.update({
+      where: { id: input.userid },
+      data: { saved: { connect: { id: input.postid } } },
+    });
+
+    return { q1, q2 };
+  }),
+
+  unsavePost: protectedProcedure.input(z.object({ postid: z.string(), userid: z.string() })).mutation(async ({ input, ctx }) => {
+    const q1 = await ctx.prisma.post.update({
+      where: { id: input.postid },
+      data: { saved: { disconnect: { id: input.userid } } },
+    });
+    const q2 = await ctx.prisma.user.update({
+      where: { id: input.userid },
+      data: { saved: { disconnect: { id: input.postid } } },
+    });
+
+    return { q1, q2 };
+  }),
+
+  setcomment: protectedProcedure.input(z.object({ postid: z.string(), userid: z.string(), parentReplyID: z.string().nullish() })).mutation(async ({ input, ctx }) => {
+    if (input.parentReplyID) {
+      const q1 = await ctx.prisma.comment.create({
+        data: {
+          user: { connect: { id: input.userid } },
+          post: { connect: { id: input.postid } },
+          parentReply: { connect: { id: input.parentReplyID } },
+        },
+      });
+
+      const q2 = await ctx.prisma.comment.update({
+        where: { id: input.parentReplyID },
+        data: {
+          replies: { connect: { id: q1.id } },
+        },
+      });
+
+      return { q1, q2 };
+    } else {
+      return ctx.prisma.comment.create({
+        data: {
+          user: { connect: { id: input.userid } },
+          post: { connect: { id: input.postid } },
+        },
+      });
+    }
+  }),
+
+  deleteComment: protectedProcedure.input(z.object({ commentid: z.string(), userid: z.string() })).mutation(({ input, ctx }) => {
+    return ctx.prisma.comment.delete({
+      where: { id: input.commentid },
+    });
+  }),
+
+  likecomment: protectedProcedure.input(z.object({ commentid: z.string(), userid: z.string() })).mutation(({ input, ctx }) => {
+    return ctx.prisma.comment.update({
+      where: { id: input.commentid },
+      data: { likes: { connect: { id: input.userid } } },
+    });
+  }),
+
+  unlikecomment: protectedProcedure.input(z.object({ commentid: z.string(), userid: z.string() })).mutation(({ input, ctx }) => {
+    return ctx.prisma.comment.update({
+      where: { id: input.commentid },
+      data: { likes: { disconnect: { id: input.userid } } },
     });
   }),
 });
