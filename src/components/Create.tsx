2aa16f8e6342c8c425e4b015ff13e5bc38b83164
Ushira Dineth <ushiradineth@gmail.com>
2023-01-19 16:55:36 +0530

    responsive changes

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -101,29 +101,30 @@ const Create = (props: itemType) => {
 
   const SelectImage = () => {
     return (
-      <div className={"absolute top-1/2 left-1/2 z-30 h-[500px] w-[400px] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl transition-all duration-700 " + data?.theme?.tertiary}>
-        <div className="flex h-12 w-full items-center justify-center border-b-[1px] border-black font-semibold">Create new post</div>
-        <div className="flex h-[93.7%] items-center justify-center">
-          <div className="grid place-items-center gap-4">
-            <svg aria-hidden="true" viewBox="0 0 36.129 36.129" className="h-52" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M30.32,7.163H1.811C0.812,7.163,0,7.974,0,8.975V31.15c0,0.998,0.812,1.811,1.811,1.811H30.32c1,0,1.812-0.812,1.812-1.811
+      
+        <div className={"absolute top-1/2 left-1/2 z-30 h-[500px] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl transition-all duration-700 " + (data?.viewport === "Mobile" ? " w-[400px] " : " w-[500px] ") + data?.theme?.tertiary}>
+          <div className="flex h-12 w-full items-center justify-center border-b-[1px] border-black font-semibold">Create new post</div>
+          <div className="flex h-[93.7%] items-center justify-center">
+            <div className="grid place-items-center gap-4">
+              <svg aria-hidden="true" viewBox="0 0 36.129 36.129" className="h-52" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M30.32,7.163H1.811C0.812,7.163,0,7.974,0,8.975V31.15c0,0.998,0.812,1.811,1.811,1.811H30.32c1,0,1.812-0.812,1.812-1.811
                 V8.974C32.129,7.972,31.32,7.163,30.32,7.163z M28.51,10.784v16.323l-4.141-5.026c-0.152-0.185-0.422-0.218-0.615-0.076
                 l-4.816,3.517l-8.27-8.045c-0.096-0.094-0.229-0.135-0.358-0.127c-0.134,0.012-0.253,0.083-0.329,0.191l-6.359,9.099V10.784H28.51
                 L28.51,10.784z M17.65,17.573c0-1.623,1.319-2.943,2.94-2.943c1.623,0,2.941,1.32,2.941,2.943c0,1.619-1.318,2.941-2.941,2.941
                 C18.969,20.514,17.65,19.191,17.65,17.573z M34.771,26.396c-0.75,0-1.356-0.608-1.356-1.356V5.88H5.206
                 c-0.75,0-1.357-0.606-1.357-1.356c0-0.749,0.607-1.356,1.357-1.356h29.565c0.75,0,1.357,0.607,1.357,1.356v20.517
                 C36.129,25.788,35.521,26.396,34.771,26.396z"
-              />
-            </svg>
-            <p className="select-none text-xl font-light">Add photos and videos here</p>
-            <div onClick={() => inputRef.current?.click()} className="flex w-[70%] cursor-pointer items-center justify-center rounded-lg bg-blue-500 py-2 text-sm font-semibold text-white hover:bg-blue-600 active:bg-blue-400">
-              Select From Computer
+                />
+              </svg>
+              <p className="select-none text-xl font-light">Add photos and videos here</p>
+              <div onClick={() => inputRef.current?.click()} className="flex w-[70%] cursor-pointer items-center justify-center rounded-lg bg-blue-500 py-2 text-sm font-semibold text-white hover:bg-blue-600 active:bg-blue-400">
+                Select From Computer
+              </div>
+              <input type="file" accept=".png, .jpg, .jpeg" ref={inputRef} onChange={(e) => setFileList(e.target.files)} style={{ display: "none" }} multiple />
             </div>
-            <input type="file" accept=".png, .jpg, .jpeg" ref={inputRef} onChange={(e) => setFileList(e.target.files)} style={{ display: "none" }} multiple />
           </div>
         </div>
-      </div>
     );
   };
 
@@ -146,7 +147,7 @@ const Create = (props: itemType) => {
     };
 
     return (
-      <div className={"absolute top-1/2 left-1/2 z-30 h-fit w-[450px] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl transition-all duration-700 " + data?.theme?.tertiary}>
+      <div className={"absolute top-1/2 left-1/2 z-30 h-fit min-h-[200px] w-fit max-w-[450px] min-w-[200px] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl " + data?.theme?.tertiary}>
         <div className="flex h-12 w-full items-center justify-center font-semibold">
           <div className="grid w-full grid-flow-col grid-cols-3 border-b-[2px] py-2">
             <div className="flex cursor-pointer items-center">
@@ -162,7 +163,7 @@ const Create = (props: itemType) => {
           <BiChevronRight onClick={() => imageIndex < files.length - 1 && setImageIndex(imageIndex + 1)} className={"fixed right-4 top-[53%] z-20 ml-auto h-4 w-4 scale-150 rounded-full bg-zinc-600 object-contain " + (imageIndex < files.length - 1 ? " cursor-pointer hover:bg-white hover:text-zinc-600 " : " opacity-0 ")} />
           <BiChevronLeft onClick={() => imageIndex > 0 && setImageIndex(imageIndex - 1)} className={"fixed left-4 top-[53%] z-20 h-4 w-4 scale-150 rounded-full bg-zinc-600 object-contain " + (imageIndex > 0 ? " cursor-pointer hover:bg-white hover:text-zinc-600 " : " opacity-0 ")} />
           <div className={"h-full w-full rounded-b-2xl object-cover"}>
-            <Cropper src={URL.createObjectURL(files[imageIndex] || new Blob())} onInteractionEnd={onInteractionEnd} onReady={onReady} className={"cropper"} defaultCoordinates={imgCoordinates.current[imageIndex]} stencilProps={options[imageIndex]?.ratio ? { aspectRatio: options[imageIndex]?.ratio || 1 } : {}} />
+            <Cropper src={URL.createObjectURL(files[imageIndex] || new Blob())} onInteractionEnd={onInteractionEnd} onReady={onReady} className={"cropper rounded-b-2xl"} defaultCoordinates={imgCoordinates.current[imageIndex]} stencilProps={options[imageIndex]?.ratio ? { aspectRatio: options[imageIndex]?.ratio || 1 } : {}} />
           </div>
           <div className="group">
             <button type="button" aria-haspopup="true" className="fixed bottom-3 left-4 cursor-pointer rounded-full bg-black p-2 text-white shadow-[0px_0px_10px_rgba(0,0,0,0.2)] transition-all duration-300 focus-within:bg-white focus-within:text-black hover:text-gray-500 hover:shadow-[0px_0px_10px_rgba(0,0,0,0.4)]">
@@ -205,7 +206,7 @@ const Create = (props: itemType) => {
 
   const Caption = () => {
     return (
-      <div className={"absolute top-1/2 left-1/2 z-30 h-fit w-[400px] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl transition-all duration-700 " + data?.theme?.tertiary}>
+      <div className={"absolute top-1/2 left-1/2 z-30 h-fit min-h-[200px] w-[400px] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl " + data?.theme?.tertiary}>
         <div className="flex h-12 w-full items-center justify-center font-semibold">
           <div className="grid w-full grid-flow-col grid-cols-3 border-b-[2px] py-2">
             <div className="flex cursor-pointer items-center">
@@ -217,19 +218,16 @@ const Create = (props: itemType) => {
             </button>
           </div>
         </div>
-        <div className="mr-4 flex">
-          <div className="flex h-[90%] w-[50%] items-center justify-center">
+        <div className="mt-[1px] flex h-fit min-h-[151px]">
+          <div className="w-[50%]">
             <NextImage src={URL.createObjectURL(files[0] || new Blob())} key="image" className="h-full w-full rounded-bl-2xl object-cover" height={500} width={400} alt={"image"} />
           </div>
-          <div className="hidden">
-            <img id="imageCrop" className="block max-w-[100%]" src={URL.createObjectURL(files[0] || new Blob())} />
-          </div>
-          <div className="ml-4 mt-2 h-12">
-            <div className="flex gap-2">
+          <div className="m-4 w-[50%]">
+            <div className="flex gap-2 truncate">
               <NextImage src={data?.user?.data.image || ""} key="image" className="h-8 w-8 rounded-full" height={100} width={100} alt={"image"} />
               {data?.user?.data.handle}
             </div>
-            <input id="post-caption" placeholder="Write a caption..." className={"m-2 mt-4 focus:outline-none " + data?.theme?.tertiary} />
+            <textarea id="post-caption" placeholder="Write a caption..." maxLength={500} className={"min-h-24 mt-4 w-full rounded-xl p-2 focus:outline-none " + data?.theme?.secondary} />
           </div>
         </div>
       </div>
