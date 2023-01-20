ce9a7a864e7970d165dc8d3732b18f5116b69769
Ushira Dineth <ushiradineth@gmail.com>
2023-01-20 13:52:21 +0530

    improved ui and added likes

--- src/pages/post/[post].tsx
+++ src/pages/post/[post].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataContext } from "../_app";
 import { useContext } from "react";
 import Head from "next/head";
@@ -9,7 +9,7 @@ import Image from "next/image";
 import { BiChevronRight, BiChevronLeft } from "react-icons/bi";
 import Spinner from "../../components/Spinner";
 import ProfileLink from "../../components/ProfileLink";
-import { AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { TbMessageCircle2 } from "react-icons/tb";
 import { IoPaperPlaneOutline } from "react-icons/io5";
 import { BsBookmark } from "react-icons/bs";
@@ -17,22 +17,42 @@ import InputBox from "../../components/InputBox";
 
 const Post = () => {
   const [imageIndex, setImageIndex] = useState(0);
+  const [showComments, setShowComments] = useState(false);
+  const [like, setLike] = useState<boolean | null>(null);
 
   const router = useRouter();
   const postID = router.query.post as string;
   const { data: session } = useSession();
   const data = useContext(DataContext);
   const post = trpc.post.getPost.useQuery({ id: postID }, { retry: false, refetchOnWindowFocus: false });
+
   const follow = trpc.user.follow.useMutation({
     onSuccess: () => {
       data?.user?.refetch();
     },
   });
+
   const unfollow = trpc.user.unfollow.useMutation({
     onSuccess: () => {
       data?.user?.refetch();
     },
   });
+
+  const likePost = trpc.post.likePost.useMutation({
+    onMutate: () => {
+      setLike(true);
+    },
+  });
+
+  const unlikePost = trpc.post.unlikePost.useMutation({
+    onMutate: () => {
+      setLike(false);
+    },
+  });
+
+  useEffect(() => {
+    post.data?.likes.forEach((e) => e.id === data?.user?.data.id && setLike(true));
+  }, [post]);
 
   const ProfileView = () => {
     return (
@@ -54,12 +74,9 @@ const Post = () => {
             </button>
           )
         }
-        user={{ userID: post.data?.user.id || "", userName: post.data?.user.name || "", userImage: post.data?.user.image || "", userHandle: post.data?.user.handle || "" }}
+        user={{ userID: post.data?.user.id || "", userName: post.data?.user.name || "", userImage: post.data?.user.image || "/image-placeholder.png", userHandle: post.data?.user.handle || "" }}
         index={0}
-        onClickHandlerPost={() => {
-          router.push({ pathname: "/" + post.data?.user.handle });
-          console.log("asd?");
-        }}
+        onClickHandlerPost={() => router.push({ pathname: "/" + post.data?.user.handle })}
       />
     );
   };
@@ -72,22 +89,37 @@ const Post = () => {
     );
   };
 
+  const Comments = () => {
+    return (
+      <div className={"grid h-[70%] w-full place-items-center border-zinc-600 " + (data?.viewport === "Mobile" ? " border-t p-4 " : " border-b ")}>
+        <p>No Comments Yet</p>
+      </div>
+    );
+  };
+
+  const InteractionBar = () => {
+    return (
+      <div className="grid grid-flow-col">
+        <div className="mt-4 grid h-fit w-fit scale-[1.6] grid-flow-col gap-2 pl-6 child-hover:text-zinc-600">
+          {like ? <AiFillHeart className="cursor-pointer text-red-500" onClick={() => unlikePost.mutate({ userid: data?.user?.data.id || "", postid: post.data?.id || "" })} /> : <AiOutlineHeart className="cursor-pointer" onClick={() => likePost.mutate({ userid: data?.user?.data.id || "", postid: post.data?.id || "" })} />}
+          <TbMessageCircle2 fill={showComments ? "white" : "none"} className="cursor-pointer" onClick={() => setShowComments(!showComments)} />
+          <IoPaperPlaneOutline className="cursor-pointer" />
+        </div>
+        <div className={"mt-4 flex h-fit w-full items-center justify-end pr-3 hover:text-zinc-600"}>
+          <BsBookmark className="scale-[1.6] cursor-pointer" />
+        </div>
+      </div>
+    );
+  };
+
   const MobileFooter = () => {
     return (
       <div className={"flex h-fit w-[400px] flex-col items-center justify-start rounded-b-2xl border-t border-zinc-600 " + (data?.viewport !== "Mobile" ? " hidden " : "") + data?.theme?.tertiary}>
         <div className="grid w-full border-zinc-600">
-          <div className="grid grid-flow-col">
-            <div className="grid h-fit w-fit scale-[1.6] grid-flow-col gap-2 pt-3 pl-6 child-hover:text-zinc-600">
-              <AiOutlineHeart />
-              <TbMessageCircle2 />
-              <IoPaperPlaneOutline />
-            </div>
-            <div className="ml-[270px] grid h-fit w-fit scale-[1.6] grid-flow-col pt-3 hover:text-zinc-600">
-              <BsBookmark />
-            </div>
-          </div>
+          <InteractionBar />
           <p className="mt-2 p-3 pb-2 font-mono text-xs text-zinc-300">{new Intl.DateTimeFormat("en-US", { month: "long" }).format(post.data?.createdAt.getMonth()).toUpperCase() + " " + post.data?.createdAt.getDate() + ", " + post.data?.createdAt.getFullYear()} </p>
         </div>
+        {showComments && <Comments />}
       </div>
     );
   };
@@ -101,27 +133,11 @@ const Post = () => {
       );
     };
 
-    const Comments = () => {
-      return (
-        <div className="grid h-[70%] w-full place-items-center border-b border-zinc-600 pb-8">
-          <p>No Comments Yet</p>
-        </div>
-      );
-    };
-
     const Interactions = () => {
       return (
-        <div className="grid h-[13%] w-full border-b border-zinc-600">
-          <div className="grid grid-flow-col">
-            <div className="grid h-fit w-fit scale-[1.6] grid-flow-col gap-2 pt-3 pl-6 child-hover:text-zinc-600">
-              <AiOutlineHeart />
-              <TbMessageCircle2 />
-              <IoPaperPlaneOutline />
-            </div>
-            <div className="ml-[220px] grid h-fit w-fit scale-[1.6] grid-flow-col pt-3 hover:text-zinc-600">
-              <BsBookmark />
-            </div>
-          </div>
+        <div className="grid h-[15%] w-full border-b border-zinc-600">
+          <InteractionBar />
+          <p className="pl-3 mt-1 text-xs text-zinc-300">{post.data?.likes.length && (post.data?.likes.length + " " + (post.data?.likes.length > 1 ? "likes" : "like"))}</p>
           <p className="pl-3 font-mono text-xs text-zinc-300">{new Intl.DateTimeFormat("en-US", { month: "long" }).format(post.data?.createdAt.getMonth()).toUpperCase() + " " + post.data?.createdAt.getDate() + ", " + post.data?.createdAt.getFullYear()} </p>
         </div>
       );
@@ -154,7 +170,7 @@ const Post = () => {
         <div className={"flex h-fit w-fit items-center justify-center p-2 transition-all duration-300 " + (data?.viewport === "Mobile" ? "  max-h-[400px] max-w-[350px]" : " max-h-[650px] max-w-[450px] ")}>
           <BiChevronLeft onClick={() => imageIndex > 0 && setImageIndex(imageIndex - 1)} className={"fixed left-4 top-[50%] h-4 w-4 scale-150 rounded-full bg-zinc-600 object-contain " + (imageIndex > 0 ? " cursor-pointer hover:bg-white hover:text-zinc-600 " : " opacity-0 ")} />
           <BiChevronRight onClick={() => imageIndex < (post.data?.imageURLs.length || 0) - 1 && setImageIndex(imageIndex + 1)} className={"fixed top-[50%] right-4 h-4 w-4 scale-150 rounded-full bg-zinc-600 object-contain " + (imageIndex < (post.data?.imageURLs.length || 0) - 1 ? " cursor-pointer hover:bg-white hover:text-zinc-600 " : " opacity-0 ")} />
-          <Image src={post.data?.imageURLs[imageIndex] || ""} key="image" className="h-full w-full object-cover" height={1000} width={1000} alt={"images"} />
+          <Image src={post.data?.imageURLs[imageIndex] || "/image-placeholder.png"} key="image" className="h-full w-full object-cover" height={1000} width={1000} alt={"images"} />
         </div>
       </div>
     );
@@ -184,7 +200,7 @@ const Post = () => {
         </Head>
         <main>
           <SignInNotification />
-          <div className={"flex h-screen select-none items-center justify-center " + data?.theme?.secondary + (data?.viewport == "Web" && session && " ml-72 ") + (data?.viewport == "Tab" && session && " ml-16 ") + (data?.viewport == "Mobile" && session && " flex-col ")}>
+          <div className={"flex h-screen select-none items-center px-2 " + data?.theme?.secondary + (data?.viewport == "Web" && session && " ml-72  justify-center ") + (data?.viewport == "Tab" && session && " ml-16 justify-center ") + (data?.viewport == "Mobile" && session && " flex-col pt-12 ")}>
             <MobileHeader />
             <PostView />
             <WebSideView />
