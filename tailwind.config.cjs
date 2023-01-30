a118162096e99e6ec473e0fb115df86520cf0e93
Ushira Dineth <ushiradineth@gmail.com>
2023-01-30 22:52:51 +0530

    added new settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,21 @@ module.exports = {
                 'md': '750px',
                 'lg': '1150px'
             },
+            animation: {
+                float: 'float 12s infinite linear',
+                'float-fast': 'float 10s infinite linear',
+                'float-fastest': 'float 8s infinite linear',
+            },
+            keyframes: {
+                float: {
+                    '0%': {
+                        transform: ' rotate(-0.001deg) translate3d(15px, 0, 0) rotate(-0.001deg)',
+                    },
+                    '100%': {
+                        transform: 'rotate(360.001deg) translate3d(15px, 0, 0) rotate(-360.001deg)',
+                    },
+                },
+            },
         },
     },
     plugins: [
