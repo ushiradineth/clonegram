b4fba1db043b2a5f5ccff714d93095610f0f8475
Ushira Dineth <ushiradineth@gmail.com>
2023-02-07 23:05:11 +0530

    fixed comment ui

--- src/pages/post/[post].tsx
+++ src/pages/post/[post].tsx
@@ -153,9 +153,9 @@ const Post = () => {
   const Comment = (props: { comment: Comment & { user: User } }) => {
     return (
       <div className="flex flex-col justify-center gap-2 p-4" key={props.comment.user.handle + props.comment.text}>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-3">
           <Link passHref href={"/profile/" + props.comment.user.handle} onClick={(e) => e.preventDefault()}>
-            <Image className={"h-fit w-12 cursor-pointer rounded-full"} onClick={() => router.push("/profile/" + props.comment.user.handle)} src={props.comment.user.image || "https://hmgdlvdpchcrxwiqomud.supabase.co/storage/v1/object/public/clonegram/Assets/image-placeholder.png"} height={160} width={160} alt="Profile Picture" priority />
+            <Image className={"h-fit w-8 cursor-pointer rounded-full"} onClick={() => router.push("/profile/" + props.comment.user.handle)} src={props.comment.user.image || "https://hmgdlvdpchcrxwiqomud.supabase.co/storage/v1/object/public/clonegram/Assets/image-placeholder.png"} height={160} width={160} alt="Profile Picture" priority />
           </Link>
           <Link passHref href={"/profile/" + props.comment.user.handle} onClick={(e) => e.preventDefault()} className="mr-2 h-fit w-56 truncate font-semibold">
             {props.comment.user.handle}
@@ -163,7 +163,7 @@ const Post = () => {
         </div>
         <div className="flex flex-col gap-2">
           <span className="break-all">{props.comment.text}</span>
-          <div className="grid w-full grid-flow-col place-items-center font-mono text-xs text-zinc-500">
+          <div className="w-fit gap-2 grid-flow-col flex items-center font-mono text-xs text-zinc-500">
             {likeComment.isLoading || unlikeComment.isLoading || post.isFetching ? <Spinner SpinnerOnly={true} size={4} /> : props.comment.likes.find((e: { id: string }) => e.id === data?.user?.data.id) ? <AiFillHeart className="cursor-pointer text-xl text-red-500" onClick={() => unlikeComment.mutate({ userid: data?.user?.data.id || "", commentid: props.comment.id })} /> : <AiOutlineHeart className="cursor-pointer text-xl" onClick={() => likeComment.mutate({ userid: data?.user?.data.id || "", commentid: props.comment.id })} />}
             {props.comment.user.handle === data?.user?.data.handle && (
               <MdOutlineDeleteOutline
