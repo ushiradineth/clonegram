5066461bb11faa9a0535cd9a40b79e3b67d08d8f
Ushira Dineth <ushiradineth@gmail.com>
2023-02-08 22:49:00 +0530

    separated comments from post

---
+++ src/components/Comment.tsx
@@ -0,0 +1,67 @@
+import { type User, type Comment } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import { trpc } from "../utils/trpc";
+import Spinner from "./Spinner";
+import { DataContext } from "../pages/_app";
+import { useContext } from "react";
+import moment from "moment";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+const Comment = (props: { setDeleteCommentID(id: string): unknown; setDeleteCommentMenu(arg0: boolean): unknown; setLikeUsers(likes: User[]): unknown; setLikesMenu(arg0: boolean): unknown; comment: Comment & { user: User } & { likes: User[] }; post: any }) => {
+  const router = useRouter();
+  const data = useContext(DataContext);
+
+  const likeComment = trpc.post.likecomment.useMutation({
+    onSuccess: () => {
+      props.post.refetch();
+    },
+  });
+
+  const unlikeComment = trpc.post.unlikecomment.useMutation({
+    onSuccess: () => {
+      props.post.refetch();
+    },
+  });
+
+  return (
+    <div className="flex flex-col justify-center gap-2 p-4" key={props.comment.user.handle + props.comment.text}>
+      <div className="flex items-center gap-3">
+        <Link passHref href={"/profile/" + props.comment.user.handle} onClick={(e) => e.preventDefault()}>
+          <Image className={"h-fit w-8 cursor-pointer rounded-full"} onClick={() => router.push("/profile/" + props.comment.user.handle)} src={props.comment.user.image || "https://hmgdlvdpchcrxwiqomud.supabase.co/storage/v1/object/public/clonegram/Assets/image-placeholder.png"} height={160} width={160} alt="Profile Picture" priority />
+        </Link>
+        <Link passHref href={"/profile/" + props.comment.user.handle} onClick={(e) => e.preventDefault()} className="mr-2 h-fit w-56 truncate font-semibold">
+          {props.comment.user.handle}
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">
+        <span className="break-all">{props.comment.text}</span>
+        <div className="flex w-fit grid-flow-col items-center gap-2 font-mono text-xs text-zinc-500">
+          {likeComment.isLoading || unlikeComment.isLoading || props.post.isFetching ? <Spinner SpinnerOnly={true} size={4} /> : props.comment.likes.find((e: { id: string }) => e.id === data?.user?.data.id) ? <AiFillHeart className="cursor-pointer text-xl text-red-500" onClick={() => unlikeComment.mutate({ userid: data?.user?.data.id || "", commentid: props.comment.id })} /> : <AiOutlineHeart className="cursor-pointer text-xl" onClick={() => likeComment.mutate({ userid: data?.user?.data.id || "", commentid: props.comment.id, commentOwnerid: props.comment.user.id, postid: props.post.data?.id || "" })} />}
+          {props.comment.user.handle === data?.user?.data.handle && (
+            <MdOutlineDeleteOutline
+              className="cursor-pointer text-xl"
+              onClick={() => {
+                props.setDeleteCommentID(props.comment.id);
+                props.setDeleteCommentMenu(true);
+              }}
+            />
+          )}
+          <button
+            onClick={() => {
+              props.setLikeUsers(props.comment.likes);
+              props.setLikesMenu(true);
+            }}
+          >
+            {(props.comment.likes.length || 0) > 0 && props.comment.likes.length + " " + ((props.comment.likes.length || 0) > 1 ? "likes" : "like")}
+          </button>
+          <p className="uppercase">{moment(props.comment.createdAt).fromNow()}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
--- src/pages/post/[post].tsx
+++ src/pages/post/[post].tsx
@@ -21,7 +21,8 @@ import ListOfUsers from "../../components/ListOfUsers";
 import UnAuthedAlert from "../../components/UnAuthedAlert";
 import moment from "moment";
 import { type Theme, toast } from "react-toastify";
-import { type Comment, type User } from "@prisma/client";
+import { type User } from "@prisma/client";
+import Comment from "../../components/Comment";
 
 const Post = () => {
   const [imageIndex, setImageIndex] = useState(0);
@@ -98,18 +99,6 @@ const Post = () => {
     },
   });
 
-  const likeComment = trpc.post.likecomment.useMutation({
-    onSuccess: () => {
-      post.refetch();
-    },
-  });
-
-  const unlikeComment = trpc.post.unlikecomment.useMutation({
-    onSuccess: () => {
-      post.refetch();
-    },
-  });
-
   useEffect(() => {
     post.data?.likes.forEach((e) => e.id === data?.user?.data.id && setLike(true));
     post.data?.saved.forEach((e) => e.id === data?.user?.data.id && setSave(true));
@@ -150,45 +139,6 @@ const Post = () => {
     );
   };
 
-  const Comment = (props: { comment: Comment & { user: User } & { likes: User[] } }) => {
-    return (
-      <div className="flex flex-col justify-center gap-2 p-4" key={props.comment.user.handle + props.comment.text}>
-        <div className="flex items-center gap-3">
-          <Link passHref href={"/profile/" + props.comment.user.handle} onClick={(e) => e.preventDefault()}>
-            <Image className={"h-fit w-8 cursor-pointer rounded-full"} onClick={() => router.push("/profile/" + props.comment.user.handle)} src={props.comment.user.image || "https://hmgdlvdpchcrxwiqomud.supabase.co/storage/v1/object/public/clonegram/Assets/image-placeholder.png"} height={160} width={160} alt="Profile Picture" priority />
-          </Link>
-          <Link passHref href={"/profile/" + props.comment.user.handle} onClick={(e) => e.preventDefault()} className="mr-2 h-fit w-56 truncate font-semibold">
-            {props.comment.user.handle}
-          </Link>
-        </div>
-        <div className="flex flex-col gap-2">
-          <span className="break-all">{props.comment.text}</span>
-          <div className="flex w-fit grid-flow-col items-center gap-2 font-mono text-xs text-zinc-500">
-            {likeComment.isLoading || unlikeComment.isLoading || post.isFetching ? <Spinner SpinnerOnly={true} size={4} /> : props.comment.likes.find((e: { id: string }) => e.id === data?.user?.data.id) ? <AiFillHeart className="cursor-pointer text-xl text-red-500" onClick={() => unlikeComment.mutate({ userid: data?.user?.data.id || "", commentid: props.comment.id })} /> : <AiOutlineHeart className="cursor-pointer text-xl" onClick={() => likeComment.mutate({ userid: data?.user?.data.id || "", commentid: props.comment.id, commentOwnerid: props.comment.user.id, postid: post.data?.id || "" })} />}
-            {props.comment.user.handle === data?.user?.data.handle && (
-              <MdOutlineDeleteOutline
-                className="cursor-pointer text-xl"
-                onClick={() => {
-                  setDeleteCommentID(props.comment.id);
-                  setDeleteCommentMenu(true);
-                }}
-              />
-            )}
-            <button
-              onClick={() => {
-                setLikeUsers(props.comment.likes);
-                setLikesMenu(true);
-              }}
-            >
-              {(props.comment.likes.length || 0) > 0 && props.comment.likes.length + " " + ((props.comment.likes.length || 0) > 1 ? "likes" : "like")}
-            </button>
-            <p className="uppercase">{moment(props.comment.createdAt).fromNow()}</p>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
   const Comments = () => {
     return (
       <div className={"flex w-full flex-col overflow-auto border-zinc-600 " + (data?.viewport === "Mobile" ? " h-fit border-t " : " h-[70%] border-b ")}>
@@ -197,7 +147,7 @@ const Post = () => {
           post.data.comments
             .slice(0)
             .reverse()
-            .map((element, index) => <Comment key={index} comment={element} />)
+            .map((element, index) => <Comment key={index} comment={element} post={post} setDeleteCommentID={setDeleteCommentID} setDeleteCommentMenu={setDeleteCommentMenu} setLikeUsers={setLikeUsers} setLikesMenu={setLikesMenu} />)
         ) : (
           <p className="flex w-full items-center justify-center p-4">No Comments Yet</p>
         )}
