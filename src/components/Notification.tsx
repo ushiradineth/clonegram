c2f0dc57188353944d1c113b6822d2457c0db0f0
Ushira Dineth <ushiradineth@gmail.com>
2023-02-08 22:33:26 +0530

    added extra notification types

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -15,7 +15,7 @@ const Notification = (props: itemType) => {
 
   const NoNotifications = (props: { text: string }) => {
     return (
-      <div className={"flex flex-col items-center justify-center rounded-2xl w-full p-8 " + data?.theme?.secondary}>
+      <div className={"flex w-full flex-col items-center justify-center rounded-2xl p-8 " + data?.theme?.secondary}>
         <div className="flex flex-col items-center justify-center p-4">
           <div className="mb-4 grid h-32 w-32 place-items-center rounded-full border-4">
             <AiOutlineHeart className="scale-x-[-6] scale-y-[6] transform" />
@@ -37,7 +37,10 @@ const Notification = (props: itemType) => {
         <Image onClick={() => onClickHandler("/profile/" + props.element.notificationCreator.handle)} className={"h-[50px] w-[50px] cursor-pointer rounded-full object-cover"} src={props.element.notificationCreator.image || "https://hmgdlvdpchcrxwiqomud.supabase.co/storage/v1/object/public/clonegram/Assets/default-user-image.jpg"} alt={String(props.index)} width={200} height={200} />
         <div>
           {props.element.type === "Follow" && <div>{props.element.notificationCreator.handle} has followed you</div>}
-          {props.element.type === "Like" && <div>{props.element.notificationCreator.handle} has liked your post</div>}
+          {props.element.type === "PostLike" && <div>{props.element.notificationCreator.handle} has liked your post</div>}
+          {props.element.type === "PostComment" && <div>{props.element.notificationCreator.handle} has commented on your post</div>}
+          {props.element.type === "CommentLike" && <div>{props.element.notificationCreator.handle} has liked your comment</div>}
+          {props.element.type === "CommentReply" && <div>{props.element.notificationCreator.handle} has replied to your comment</div>}
         </div>
         {props.element.type === "Like" && <Image onClick={() => onClickHandler("/post/" + props.element.post.id)} className={"h-[50px] w-[50px] cursor-pointer rounded-2xl  object-cover"} src={props.element.post.imageURLs[0] || "https://hmgdlvdpchcrxwiqomud.supabase.co/storage/v1/object/public/clonegram/Assets/image-placeholder.png"} alt={String(props.index)} width={200} height={200} />}
       </div>
