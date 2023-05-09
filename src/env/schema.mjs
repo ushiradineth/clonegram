ccb261185805645feab880499d96ad6be328e936
Ushira Dineth <ushiradineth@gmail.com>
2023-05-09 22:37:22 +0530

    feat(prisma): added edge proxy data

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 export const serverSchema = z.object({
     DATABASE_URL: z.string().url(),
+    DIRECT_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET: process.env.NODE_ENV === "production" ? z.string().min(1) : z.string().min(1).optional(),
     NEXTAUTH_URL: z.preprocess(
